ca38bc61bafff9fa3e2a77bcf64f4d969854a079
cchang373 <30272503+cchang373@users.noreply.github.com>
2017-08-17 14:54:57 -0400

    Update convert.py

--- hermes/convert.py
+++ hermes/convert.py
@@ -2,17 +2,55 @@ import json
 from collections import OrderedDict
 import os
 import datetime
-from espresso import espresso
-
+import numpy as np
+from spglib import spglib
+from ase import Atoms,Atom
+from ase.constraints import dict2constraint
 # Functions for data transformations not involving database
 
 
 ## Functions for creating data objects from ASE atoms objects
 def atoms_to_dict(atoms):
-    return {}
+     d=OrderedDict(atoms=[{'symbol':atom.symbol,
+                          'position':atom.position,
+                          'tag':atom.tag,
+                          'index':atom.index,
+                          'charge':atom.charge,
+                          'momentum':atom.momentum,
+                          'magmom':atom.magmom}
+    for atom in atoms],
+    cell=atoms.cell,
+    pbc=atoms.pbc,
+    info=atoms.info,
+    constraints=[c.todict() for c in atoms.constraints])
+    
+    d['natoms']=len(atoms)
+    cell=atoms.get_cell()
+    if cell is not None and np.linalg.det(cell) > 0:
+        d['volume']=atoms.get_volume()
+        
+    d['mass']=sum(atoms.get_masses())
+    syms=atoms.get_chemical_symbols()
+    d['chemical_symbols']=list(set(syms))
+    d['spacegroup']=spglib.get_spacegroup(atoms)
+    
+    return d
 
 def dict_to_atoms(d):
-    return {}
+    atoms=Atoms([Atom(symbol=atom['symbol'],
+                      position=atom['position'],
+                      tag=atom['tag'],
+                      index=atom['index'],
+                      charge=atom['charge'],
+                      momentum=atom['momentum'],
+                      magmom=atom['magmom'])
+                      for atom in d['atoms']['atoms']],
+                          cell=d['atoms']['cell'],
+                          pbc=d['atoms']['pbc'],
+                          info=d['atoms']['info'],
+                          mass=d['atoms']['mass'],
+                          constraint=[dict2constraint(c) for c in d['atoms']['constraints']])
+    return atoms
 
 ## Functions for creating data objects from calculator objects
 
@@ -28,8 +66,7 @@ def calc_to_dict(calc):
        raise IOError('calculator does not have todict imprementation')
     return d
 def dict_to_calc(calc_dict):
-    d = espresso(**calc_dict)
-    return d
+    pass
 
 ## Functions for transforming data between dictionary, json, etc.
 
