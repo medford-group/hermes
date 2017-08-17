af8aec055d8a617b8cd6ace7d92bfe81a1e85223
benjamin <benjamin@benjamin-ThinkCentre-M900>
2017-08-17 14:44:16 -0400

    data to dict

--- hermes/convert.py
+++ hermes/convert.py
@@ -1,4 +1,7 @@
 import json
+from collections import OrderedDict
+import os
+import datetime
 
 # Functions for data transformations not involving database
 
@@ -16,6 +19,8 @@ def calc_to_dict(calc):
     # check to see if calc is espresso
     try:
        calc = calc.get_calculator()
+    except:
+        pass
     try:
        d = calc.todict()
     except:
@@ -26,7 +31,16 @@ def calc_to_dict(calc):
 ## Functions for transforming data between dictionary, json, etc.
 
 def data_to_dict(atoms, calc, metadata):
-    d = {}
+    
+    atom_dict = atoms_to_dict(atoms)
+    calc_dict = calc_to_dict(calc)
+    if 'name' not in calc_dict.keys():
+        calc_dict['name']= calc.__class__.__name__
+    metadata_dict = OrderedDict(metadata)
+    metadata_dict = ID_stamper(metadata_dict)
+    metadata_dict['mtime']=datetime.datetime.utcnow()
+    d = OrderedDict(atom_dict,calc_dict,metadata_dict)
+    
     return d
 
 def dict_to_data(d):
@@ -43,3 +57,10 @@ def data_from_json(j):
     json_data = json.load(json_file)
     a, c, m = from_dict(json_data)
 
+
+def ID_stamper(metadata_dict):
+    if 'USER' not in metadata_dict:
+        metadata_dict['USER']=os.getenv('USER')
+    if 'ctime' not in metadata_dict:
+        metadata_dict['ctime']=datetime.datetime.utcnow()
+    return metadata_dict
